3d03aadf19e257db8d00eaf0ebd50fea0cfa1f5e
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2021-12-09 05:46:04 +0200

    add( Header.vue ): header component

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+  .header
+    .header__innerText
+      |WELCOME TO VISEVEN
+      span  IMAGESTOCK
+</template>
+
+<script>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style scoped>
+
+</style>
