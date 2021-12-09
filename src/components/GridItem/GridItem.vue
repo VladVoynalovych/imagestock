6e72846e3cc67601e5513f7016dcf570e7bf9864
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2021-12-09 10:18:40 +0200

    update(*): grid styles, grid item(component styles, images), header styles, image for check

---
+++ src/components/GridItem/GridItem.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+  .gridItem
+    img.gridItem__itemImage(src='~@/assets/media/images/ciri.jpg')
+    .gridItem__infoArea
+      .gridItem__comments.gridItem__icon
+        .gridItem__valueWrapper 5
+
+      .gridItem__dislikes.gridItem__icon
+        .gridItem__valueWrapper 5
+
+      .gridItem__likes.gridItem__icon
+        .gridItem__valueWrapper 5
+
+</template>
+
+<script>
+export default {
+  name: 'GridItem',
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -2,17 +2,29 @@
   .mainLayout
     Header
     Grid
+      GridItem
+      GridItem
+      GridItem
+      GridItem
+      GridItem
+      GridItem
+      GridItem
+      GridItem
+      GridItem
+      GridItem
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Header from '../../components/Header/Header.vue';
 import Grid from '../../components/Grid/Grid.vue';
+import GridItem from '../../components/GridItem/GridItem.vue';
 
 @Component({
   components: {
     Header,
     Grid,
+    GridItem,
   },
 })
 export default class MainLayout extends Vue {
