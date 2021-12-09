6dcce525b0f97b5610b205e9e62602fbf6c6a933
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2021-12-09 06:47:36 +0200

    update(*): grid component, grid styles

---
+++ src/components/Grid/Grid.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  .grid
+    slot
+</template>
+
+<script>
+export default {
+  name: 'Grid',
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -1,15 +1,18 @@
 <template lang="pug">
   .mainLayout
     Header
+    Grid
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import Header from '../../components/Header/Header.vue';
+import Grid from '../../components/Grid/Grid.vue';
 
 @Component({
   components: {
     Header,
+    Grid,
   },
 })
 export default class MainLayout extends Vue {
