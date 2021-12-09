b193bb4f8370189dded52a88efdc62f20421c85b
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2021-12-09 05:46:29 +0200

    update( MainLayout.vue ): header component

--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -1,11 +1,19 @@
 <template lang="pug">
-  div hello world
+  .mainLayout
+    Header
 </template>
 
-<script>
-export default {
-  name: 'MainLayout',
-};
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Header from '../../components/Header/Header.vue';
+
+@Component({
+  components: {
+    Header,
+  },
+})
+export default class MainLayout extends Vue {
+}
 </script>
 
 <style scoped>
