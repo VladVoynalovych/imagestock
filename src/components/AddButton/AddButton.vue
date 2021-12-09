937ff0aecad780cc96722b5b7542a6d48596df90
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2021-12-09 11:05:01 +0200

    add(AddButton): component, markup, styles, images

---
+++ src/components/AddButton/AddButton.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  .addButton.gridItem
+    .addButton__iconWrapper
+      img.addButton__icon(src='~@/assets/media/images/AddButton/plus_icon.png')
+      span.addButton__text
+        |Add your
+        |Picture
+</template>
+
+<script>
+export default {
+  name: 'AddButton',
+};
+</script>
+
+<style scoped>
+
+</style>
