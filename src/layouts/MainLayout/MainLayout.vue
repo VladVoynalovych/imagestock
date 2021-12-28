a00f81cccab9f33906aaf947facaf85b6481676d
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2021-12-28 09:38:14 +0200

    update( MainLayout.vue ): template, functionality

--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -2,27 +2,41 @@
   .mainLayout
     Header
     Grid
-      GridItem(@click.native="popupOpened = !popupOpened")
-      GridItem(@click.native="popupOpened = !popupOpened")
-      GridItem(@click.native="popupOpened = !popupOpened")
-      GridItem(@click.native="popupOpened = !popupOpened")
-      GridItem(@click.native="popupOpened = !popupOpened")
-      GridItem(@click.native="popupOpened = !popupOpened")
-      GridItem(@click.native="popupOpened = !popupOpened")
-      GridItem(@click.native="popupOpened = !popupOpened")
-      GridItem(@click.native="popupOpened = !popupOpened")
+      GridItem(
+          v-for="(image, index) in getAllImages"
+          :key="index"
+          :src="image.src"
+          :likesCount="image.likes"
+          :dislikesCount="image.dislikes"
+          :commentsCount="image.comments.length"
+          @click.native="openPopup(index)"
+      )
       AddButton
 
-    ImagePopup(:opened.sync="popupOpened")
+    ImagePopup(
+        v-if="getAllImages[currentImageId]"
+        :opened.sync="popupOpened"
+        :likesCount="getAllImages[currentImageId].likes"
+        :dislikesCount="getAllImages[currentImageId].dislikes"
+        :comments="getAllImages[currentImageId].comments"
+        :src="getAllImages[currentImageId].src"
+        :itemIndex="currentImageId"
+      )
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
 import Header from '../../components/Header/Header.vue';
 import Grid from '../../components/Grid/Grid.vue';
 import GridItem from '../../components/GridItem/GridItem.vue';
 import AddButton from '../../components/AddButton/AddButton.vue';
 import ImagePopup from '../../components/ImagePopup/ImagePopup.vue';
+import { galleryActions, galleryGetters } from '@/store/modules/gallery/publicConstants';
+
+import { IGalleryItem } from '@/interfaces/gallery';
+
+const galleryModule = namespace('galleryModule');
 
 @Component({
   components: {
@@ -34,7 +48,24 @@ import ImagePopup from '../../components/ImagePopup/ImagePopup.vue';
   },
 })
 export default class MainLayout extends Vue {
+  @galleryModule.Action(galleryActions.UPLOAD_IMAGES)
+  private uploadImages!: () => void;
+
+  @galleryModule.Getter(galleryGetters.GET_ALL_IMAGES)
+  private getAllImages!: Array<IGalleryItem>;
+
   private popupOpened = false;
+
+  private currentImageId = 0;
+
+  private mounted(): void {
+    this.uploadImages();
+  }
+
+  private openPopup(index:number): void {
+    this.currentImageId = index;
+    this.popupOpened = !this.popupOpened;
+  }
 }
 </script>
 
