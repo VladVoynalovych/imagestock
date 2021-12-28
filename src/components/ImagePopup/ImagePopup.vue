b9cc7a51d8e0627ea0b3488dd79c9d6aa280f16a
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2021-12-28 09:37:18 +0200

    update( ImagePopup.vue ): popup functionality

--- src/components/ImagePopup/ImagePopup.vue
+++ src/components/ImagePopup/ImagePopup.vue
@@ -7,85 +7,157 @@
             @click="close"
           )
         .imagePopup__imageWrapper
-          img.imagePopup__image(src='~@/assets/media/images/ciri.jpg')
+          img.imagePopup__image(:src="src")
           .imagePopup__statsWrapper.border-box
             .imagePopup__buttonsWrapper
-              .imagePopup__statBtn.imagePopup__dislike.border-box
+              .imagePopup__statBtn.imagePopup__dislike.border-box(@click="dislike")
                 .imagePopup__btnIcon.imagePopup__iconDislike
-                  .imagePopup__btnTextWrapper 5
-              .imagePopup__statBtn.imagePopup__like.border-box
+                  .imagePopup__btnTextWrapper {{ dislikesCount }}
+              .imagePopup__statBtn.imagePopup__like.border-box(@click="like")
                 .imagePopup__btnIcon.imagePopup__iconLike
-                  .imagePopup__btnTextWrapper 5
+                  .imagePopup__btnTextWrapper {{ likesCount }}
 
         .imagePopup__commentsWrapper.border-box
-          .imagePopup__commentsCount Comments: 5
+          .imagePopup__commentsCount(v-if="comments.length > 0") Comments: {{ comments.length }}
           .imagePopup__commentsScroll
-            .imagePopup__commentWrapper
+            .imagePopup__commentWrapper(
+                v-for="(comment, index) in comments"
+                :key="index"
+              )
               .imagePopup__commentInfo.border-box
-                .imagePopup__userName By Anon93
-                .imagePopup__commentDate Today 10:21 PM
-              .imagePopup__comment.border-box
-                |Nullam viverra leo eget urna maximus,
-                |et pellentesque enim volutpat.
-
-            .imagePopup__commentWrapper
-              .imagePopup__commentInfo.border-box
-                .imagePopup__userName By Andrey
-                .imagePopup__commentDate 5 days ago 5:21 PM
-              .imagePopup__comment.border-box
-                |Integer velit lectus, tincidunt rhoncus
-                |nunc ut, dapibus dignissim arcu.
-
-            .imagePopup__commentWrapper
-              .imagePopup__commentInfo.border-box
-                .imagePopup__userName By Sergey
-                .imagePopup__commentDate 6 days ago 3:21 PM
-              .imagePopup__comment.border-box
-                |Quisque egestas cursus ipsum, vitae
-                |commodo nunc varius nec.
-
-            .imagePopup__commentWrapper
-              .imagePopup__commentInfo.border-box
-                .imagePopup__userName By Misha
-                .imagePopup__commentDate 7 days ago 4:22 AM
-              .imagePopup__comment.border-box
-                |Donec ut justo nulla. Duis posuere in
-                |leo in congue.
-
-            .imagePopup__commentWrapper
-              .imagePopup__commentInfo.border-box
-                .imagePopup__userName By Irina
-                .imagePopup__commentDate 8 days ago 3:21 AM
-              .imagePopup__comment.border-box
-                |Quisque convallis nec est nec maximus.
-                |Sed lacus est, congue ac metus vel,
-                |lacinia interdum nisl. Integer sit amet
-                |faucibus sapien, vitae dictum sapien.
-                |Maecenas ac nunc ut libero laoreet
-                |porta.
+                .imagePopup__userName {{ comment.userName }}
+                .imagePopup__commentDate {{ getDate(comment.date) }}
+              .imagePopup__comment.border-box {{ comment.text }}
           .imagePopup__divider
           .imagePopup__newCommentWrapper
             input(
                 type='text'
                 placeholder='Type your nickname here...'
                 class='imagePopup__comentatorName border-box'
+                v-model="userName"
               )
 
             .imagePopup__commentArea
-              textarea.imagePopup__textArea.border-box(placeholder="Write your comment here...")
-              .imagePopup__btnSubmit
+              textarea.imagePopup__textArea.border-box(
+                  placeholder="Write your comment here..."
+                  v-model="commentText"
+                )
+              .imagePopup__btnSubmit(@click="sendComment")
 
 </template>
 
 <script lang="ts">
+import { getDateString } from 'auxiliary-functions';
 import { Component, Prop, Vue } from 'vue-property-decorator';
+import { namespace } from 'vuex-class';
+import { IUserComment } from '@/interfaces/gallery';
+import { galleryActions } from '@/store/modules/gallery/publicConstants';
+
+const galleryModule = namespace('galleryModule');
 
   @Component
 export default class ImagePopup extends Vue {
     @Prop() private opened!: boolean;
 
+    @Prop() private itemIndex!: number;
+
+    @Prop({ default: 0 }) private src!: string;
+
+    @Prop({ default: 0 }) private likesCount!: number;
+
+    @Prop({ default: 0 }) private dislikesCount!: number;
+
+    @Prop({ default: () => [] }) private comments!: Array<IUserComment>;
+
+    protected userActivity = {
+      isLiked: false,
+      isDisliked: false,
+    };
+
+    protected userName = '';
+
+    protected commentText = '';
+
+    @galleryModule.Action(galleryActions.LIKES_INCREMENTATION)
+    private likesIncrementation!: (index:number) => void;
+
+    @galleryModule.Action(galleryActions.DISLIKES_INCREMENTATION)
+    private dislikesIncrementation!: (index:number) => void;
+
+    @galleryModule.Action(galleryActions.LIKES_DECREMENT)
+    private likesDecrement!: (index:number) => void;
+
+    @galleryModule.Action(galleryActions.DISLIKES_DECREMENT)
+    private dislikesDecrement!: (index:number) => void;
+
+    @galleryModule.Action(galleryActions.UPDATE_LOCAL_STORAGE)
+    private updateLocalStorage!: () => void;
+
+    @galleryModule.Action(galleryActions.COMMENT_INCREMENT)
+    private commentIncrement!: (commentData:object) => void;
+
+    protected like():void {
+      if (!this.userActivity.isDisliked) {
+        if (!this.userActivity.isLiked) {
+          this.userActivity.isLiked = !this.userActivity.isLiked;
+          this.likesIncrementation(this.itemIndex);
+        }
+      } else {
+        this.userActivity.isLiked = !this.userActivity.isLiked;
+        this.userActivity.isDisliked = !this.userActivity.isDisliked;
+        this.dislikesDecrement(this.itemIndex);
+        this.likesIncrementation(this.itemIndex);
+      }
+
+      this.updateLocalStorage();
+    }
+
+    protected dislike():void {
+      if (!this.userActivity.isLiked) {
+        if (!this.userActivity.isDisliked) {
+          this.userActivity.isDisliked = !this.userActivity.isDisliked;
+          this.dislikesIncrementation(this.itemIndex);
+        }
+      } else {
+        this.userActivity.isLiked = !this.userActivity.isLiked;
+        this.userActivity.isDisliked = !this.userActivity.isDisliked;
+        this.likesDecrement(this.itemIndex);
+        this.dislikesIncrementation(this.itemIndex);
+      }
+
+      this.updateLocalStorage();
+    }
+
     close():void {
-      this.opened = !this.opened;
+      this.userActivity = {
+        isDisliked: false,
+        isLiked: false,
+      };
+      this.$emit('update:opened', false);
+    }
+
+    clearInputs():void {
+      this.userName = '';
+      this.commentText = '';
+    }
+
+    sendComment():void {
+      const commentData = {
+        itemIndex: this.itemIndex,
+        comment: {
+          userName: this.userName,
+          text: this.commentText,
+          date: new Date(),
+        },
+      };
+
+      this.commentIncrement(commentData);
+      this.clearInputs();
+      this.updateLocalStorage();
+    }
+
+    protected getDate(commentDate:Date):string {
+      return getDateString(new Date(commentDate));
     }
 }
 </script>
