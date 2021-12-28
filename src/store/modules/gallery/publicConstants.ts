3edfc648261167ee492950d7c1cba7933b078399
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2021-12-28 05:21:33 +0200

    add( modules/gallery/publicConstants ): getters, mutations, actions names

---
+++ src/store/modules/gallery/publicConstants.ts
@@ -0,0 +1,14 @@
+export const galleryGetters = {
+  GET_ALL_IMAGES: 'GET_ALL_IMAGES',
+  GET_IMAGE_BY_ID: 'GET_IMAGE_BY_ID',
+};
+
+export const galleryActions = {
+  UPLOAD_IMAGES: 'UPLOAD_IMAGES',
+};
+
+export const galleryMutations = {
+  ADD_LIKE: 'ADD_LIKE',
+  ADD_DISLIKE: 'ADD_DISLIKE',
+  ADD_COMMENT: 'ADD_COMMENT',
+};
