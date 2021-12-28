ed4a1b384725798b3dc628bf08a76b9fb8084213
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2021-12-28 09:36:46 +0200

    update( GridItem.vue ): props

--- src/components/GridItem/GridItem.vue
+++ src/components/GridItem/GridItem.vue
@@ -1,22 +1,31 @@
 <template lang="pug">
   .gridItem
-    img.gridItem__itemImage(src='~@/assets/media/images/ciri.jpg')
+    img.gridItem__itemImage(:src="src")
     .gridItem__infoArea
       .gridItem__comments.gridItem__icon
-        .gridItem__valueWrapper 5
+        .gridItem__valueWrapper {{ likesCount }}
 
       .gridItem__dislikes.gridItem__icon
-        .gridItem__valueWrapper 5
+        .gridItem__valueWrapper {{ dislikesCount }}
 
       .gridItem__likes.gridItem__icon
-        .gridItem__valueWrapper 5
+        .gridItem__valueWrapper {{ commentsCount }}
 
 </template>
 
-<script>
-export default {
-  name: 'GridItem',
-};
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+  @Component
+export default class GridItem extends Vue {
+    @Prop({ default: 0 }) private likesCount!: number;
+
+    @Prop({ default: 0 }) private dislikesCount!: number;
+
+    @Prop({ default: 0 }) private commentsCount!: number;
+
+    @Prop() private src!: string;
+}
 </script>
 
 <style scoped>
