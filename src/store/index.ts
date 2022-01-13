718695c6995f73073c971c27edf8e209b55b1fb2
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2022-01-13 05:34:26 +0200

    add( store/index.ts ): AddImagePopup module connection

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import galleryModule from './modules/gallery/index';
+import addImagePopupModule from './modules/addImagePopup/index';
 
 Vue.use(Vuex);
 
@@ -14,5 +15,6 @@ export default new Vuex.Store({
   },
   modules: {
     galleryModule,
+    addImagePopupModule,
   },
 });
