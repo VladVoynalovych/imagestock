987a7b6bc5470d8f8e3a80e7ce138581c02742dd
Vlad Voynalovych <vlad.voynalovich@gmail.com>
2022-01-31 02:45:30 +0200

    update( AddImagePopup.vue ): add image into gallery functionality, mark already added and new added images functionality

--- src/components/AddImagePopup/AddImagePopup.vue
+++ src/components/AddImagePopup/AddImagePopup.vue
@@ -21,13 +21,20 @@
           v-show="showDisclaimer"
         ) There will be your images
 
-        .addImagePopup__imageContainer(
+        .addImagePopup__imageContainer.border-box(
           v-if="getAllImages.length"
           v-for="(item, index) in getAllImages"
+          :class="{'addImagePopup__imageContainer_alreadyAdded': isBelongToGallery(item.src),'addImagePopup__imageContainer_newAdded': item.added }"
         )
           img(
             :src='item.src'
             :key="index"
+            class='addImagePopup__image'
+          )
+          img(
+            src='~@/assets/media/images/AddButton/plus_icon.png'
+            class='plusButton'
+            @click.once="addImage(item.src, index)"
           )
       img.popup__closeButton(
         src='~@/assets/media/images/ImagePopup/close_icon.png'
@@ -39,9 +46,14 @@
 import { Component, Prop, Emit, Vue } from 'vue-property-decorator';
 import { namespace } from 'vuex-class';
 import { popupActions, popupGetters } from '@/store/modules/addImagePopup/publicConstants';
+import { galleryActions, galleryGetters } from '@/store/modules/gallery/publicConstants';
+
 import { INewImage } from '@/interfaces/flickr';
+import { IGalleryItem } from '@/interfaces/gallery';
 
 const addImagePopupModule = namespace('addImagePopupModule');
+
+const galleryModule = namespace('galleryModule');
 
   @Component
 export default class AddImagePopup extends Vue {
@@ -63,8 +75,22 @@ export default class AddImagePopup extends Vue {
     @addImagePopupModule.Action(popupActions.CLEAR_IMAGES_POPUP)
     private clearImages!: () => void;
 
+    @addImagePopupModule.Action(popupActions.SET_AS_ADDED)
+    private setAsAdded!: (id:number) => void;
+
     @addImagePopupModule.Getter(popupGetters.GET_UPLOADED_IMAGES)
     private getAllImages!: Array<INewImage>;
+
+    // gallery module functionality
+    @galleryModule.Getter(galleryGetters.GET_ALL_IMAGES)
+    private getAllGalleryImages!: Array<IGalleryItem>;
+
+    @galleryModule.Action(galleryActions.ADD_IMAGE_TO_GALLERY)
+    private addImageToGallery!: (src:string) => void;
+
+    @galleryModule.Action(galleryActions.UPDATE_LOCAL_STORAGE)
+    private updateLocalStorage!: () => void;
+    // gallery module functionality
 
     private uploadImages(keyWord:string):void {
       if (keyWord !== '') {
@@ -74,6 +100,16 @@ export default class AddImagePopup extends Vue {
       } else {
         alert('The search string shouldn`t be empty');
       }
+    }
+
+    private addImage(src:string, index:number):void {
+      this.addImageToGallery(src);
+      this.setAsAdded(index);
+      this.updateLocalStorage();
+    }
+
+    private isBelongToGallery(src:string) {
+      return !!this.getAllGalleryImages.find(item => item.src === src);
     }
 }
 </script>
